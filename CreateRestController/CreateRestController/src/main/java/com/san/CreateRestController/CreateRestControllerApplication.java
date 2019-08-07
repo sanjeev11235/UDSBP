109b59ea32ccc781e1a5a3b35cf693aa596f80ed
Sanjeev Saini <sanjeevsaini11235@gmail.com>
2019-08-07 11:04:02 +0530

    st p3

---
+++ CreateRestController/CreateRestController/src/main/java/com/san/CreateRestController/CreateRestControllerApplication.java
@@ -0,0 +1,13 @@
+package com.san.CreateRestController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateRestControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateRestControllerApplication.class, args);
+	}
+
+}
---
+++ InitializerDemo/InitializerDemo/src/main/java/com/san/InitializerDemo/InitializerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.san.InitializerDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitializerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitializerDemoApplication.class, args);
+	}
+
+}
